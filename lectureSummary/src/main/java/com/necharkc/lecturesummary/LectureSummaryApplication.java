827dd84a461252380d790ee9e43f3aedb15e7661
SpeedRelativity <necharkc5150@gmail.com>
2025-10-11 18:20:13 -0700

    init project

---
+++ lectureSummary/src/main/java/com/necharkc/lecturesummary/LectureSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.necharkc.lecturesummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureSummaryApplication.class, args);
+    }
+
+}

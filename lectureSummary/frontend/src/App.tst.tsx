44fcfd439c1360d969a691196ee5786fa7d50682
SpeedRelativity <necharkc5150@gmail.com>
2025-10-13 16:23:33 -0700

    frontend part done, need to work on backend next

--- lectureSummary/frontend/src/App.tst.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders upload heading", () => {
-  render(<App />);
-  expect(
-    screen.getByText(/Upload Your File or Paste a Youtube Link/i)
-  ).toBeInTheDocument();
-});
---
+++ lectureSummary/frontend/src/pages/Processing.tsx
@@ -0,0 +1,9 @@
+const Processing = () => {
+  return (
+    <div>
+      <h1 className="">Processing...</h1>
+    </div>
+  );
+};
+
+export default Processing;

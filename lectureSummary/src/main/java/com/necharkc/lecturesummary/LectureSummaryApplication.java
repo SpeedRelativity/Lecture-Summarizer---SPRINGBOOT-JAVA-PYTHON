f9507db9bef95f415f3face34b085d0c34652901
SpeedRelativity <necharkc5150@gmail.com>
2025-10-14 18:51:48 -0700

    handing http requests

--- lectureSummary/src/main/java/com/necharkc/lecturesummary/LectureSummaryApplication.java
+++ lectureSummary/src/main/java/com/necharkc/lecturesummary/LectureSummaryApplication.java
@@ -2,12 +2,23 @@ package com.necharkc.lecturesummary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LectureSummaryApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LectureSummaryApplication.class, args);
+
+
+    }
+
+    @GetMapping("/submission")
+    public void startPythonWorker(){
+        System.out.println("Lecture Summary Application started");
     }
 
 }
